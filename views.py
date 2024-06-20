7efedbbfd65d0950404d86d7ca0365be746965f5
PREETHINRPREETHINR <139801521+PREETHINRPREETHINR@users.noreply.github.com>
2024-06-20 10:20:53 -0400

    Add files via upload

---
+++ views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+
+def HomePage(request):
+    user = request.user
+    return render(request, 'home.html',{'user':user})
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        
+        if pass1!=pass2:
+            return HttpResponse("Your password and conform password aren't same")
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+    return render(request,'signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("Your username or password is incorrect!!")
+    return render(request,'login.html')
